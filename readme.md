de7a55d4038909ae55c67207eb702480146080f0
roman <s91630277@gmail.com>
2020-01-30 23:33:03 +0400

    test: Moved old tests

--- readme.md
+++ readme.md
@@ -2,3 +2,19 @@
 
 Dependency injection implementation in TypeScript,
 based on [nehalist.io/dependency-injection-in-typescript](https://nehalist.io/dependency-injection-in-typescript).
+
+## Install
+
+```sh
+npm install injectant --save
+```
+
+## Peer Dependencies
+
+1. Reflection (any of):
+
+-   [reflect-metadata](https://github.com/rbuckton/reflect-metadata) is ~50K
+-   [core-js/es7/reflect](https://github.com/zloirock/core-js) is ~80K
+-   [@abraham/reflection](https://github.com/abraham/reflection) is ~3K
+
+2. tslib
--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,5 +1,5 @@
 import '@abraham/reflection';
-import { Injectable, Injector } from '.';
+import { Injectable, Injector, Service } from '.';
 
 it('smoke', () => {
     expect(Injectable).toBeTruthy();
@@ -30,4 +30,40 @@ it('three tier', () => {
 
     const controller = Injector.resolve(ZooController);
     expect(controller.go()).toEqual('Welcome to park');
+});
+
+describe('Injector', () => {
+    @Service()
+    class Foo {}
+
+    @Service()
+    class Bar {}
+
+    @Service()
+    class Foobar {
+        constructor(public foo: Foo, public bar: Bar) {}
+    }
+
+    @Service()
+    class Baz {
+        constructor(public foobar: Foobar) {}
+    }
+
+    it('should create simple instances', () => {
+        let foo = Injector.resolve(Foo);
+        expect(foo).toBeInstanceOf(Foo);
+    });
+
+    it('should create dependency injected instances', () => {
+        let foobar = Injector.resolve(Foobar);
+        expect(foobar.foo).toBeInstanceOf(Foo);
+        expect(foobar.bar).toBeInstanceOf(Bar);
+    });
+
+    it('should create deep dependency injected instances', () => {
+        let baz = Injector.resolve(Baz);
+        expect(baz.foobar).toBeInstanceOf(Foobar);
+        expect(baz.foobar.foo).toBeInstanceOf(Foo);
+        expect(baz.foobar.bar).toBeInstanceOf(Bar);
+    });
 });
